6cdf0dbb90c376932a9f4196118e2d8dbc1215e0
chen <23054115+cwkang1998@users.noreply.github.com>
2022-12-17 01:37:02 +0800

    feat: complete 02-private-inputs-and-hash-functions

---
+++ 02-private-inputs-and-hash-functions/src/IncrementalSecret.test.ts
@@ -0,0 +1,7 @@
+// import { IncrementalSecret } from './IncrementalSecret';
+
+describe('IncrementalSecret.js', () => {
+  describe('IncrementalSecret()', () => {
+    it.todo('should be correct');
+  });
+});
---
+++ 02-private-inputs-and-hash-functions/src/index.ts
@@ -0,0 +1,3 @@
+import { IncrementSecret } from './IncrementalSecret.js';
+
+export { IncrementSecret };
